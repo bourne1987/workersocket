5adb9b135450ac2bcd2d7937c52812ac4942dd3d
lane <61647649@qq.com>
2016-09-07 19:29:09 +0800

    modify

--- test/TestClient.php
+++ test/TestClient.php
@@ -1,21 +1,35 @@
 <?php
 include_once "../src/start.php";
-$client = new WorkerClient("tcp", WorkerClient::SOCKET_SYNC);
-$client->connect("127.0.0.1", 9501, 1);
-$client->send("aaaa");
-var_dump($client->recv());
-$client->close();
+
+
+//$client = new WorkerClient("json", WorkerClient::SOCKET_ASYNC, false);
+//$client->connect("127.0.0.1", 9501);
+//$client->send("aaaa1");
+//var_dump($client->recv());
+//$client->send("aaaa2");
+//var_dump($client->recv());
+//$client->send("aaaa3");
+//var_dump($client->recv());
+//$client->send("aaaa4");
+//var_dump($client->recv());
+//$client->close();
 
 //exit;
-//$cli = new WorkerClient('json', WorkerClient::SOCKET_ASYNC);
-//$cli->on('connect', function($cli) {
-    //echo "链接上了.\n";
-    //$cli->send("hhhhhhhhh");
-//});
-//$cli->on('receive', function($cli, $data) {
-    //echo "收到数据:".serialize($data)."\n";
-//});
-//$cli->on('close', function($cli) {
-    //echo "服务端链接关闭了，客户端也需要关闭.\n";
-//});
-//$cli->connect('127.0.0.1', '9501');
+$cli = new WorkerClient('json', WorkerClient::SOCKET_ASYNC);
+$cli->on('connect', function($cli) {
+    echo "链接上了.\n";
+    $cli->send("hhhhhhhhh");
+    //$cli->tick(1, function($timerId) use ($cli) {
+        //$cli->send("hhhhhhhhh");
+    //});
+});
+
+$cli->on('receive', function($cli, $data) {
+    echo "收到数据:".serialize($data)."\n";
+});
+
+$cli->on('close', function($cli) {
+    echo "服务端链接关闭了，客户端也需要关闭.\n";
+});
+
+$cli->connect('127.0.0.1', '9501');
